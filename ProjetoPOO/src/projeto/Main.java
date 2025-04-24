4fc4d735c7c64162a02495966bfda7bbe73da366
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2025-04-23 22:19:03 -0300

    Add files via upload

---
+++ ProjetoPOO/src/projeto/Main.java
@@ -0,0 +1,12 @@
+package projeto;
+
+public class Main {
+    public static void main(String[] args) {
+        Descoberta descoberta = new Descoberta();
+        Palavras palavras = new Palavras();
+
+
+        descoberta.inciarJogo();
+
+    }
+}
